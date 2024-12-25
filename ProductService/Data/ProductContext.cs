116f55ea00fb2064cebe07c646a18478eeaaaf42
Dani <dany12rp@hotmail.com>
2024-12-25 14:32:21 -0300

    Cadena de conexion y creacion automatica de base de datos

--- ProductService/Data/ProductContext.cs
+++ ProductService/Data/ProductContext.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using ProductService.Models;
 
 namespace ProductService.Data
 {
     public class ProductContext : DbContext
     {
-        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) 
+        {
+            try
+            {
+                var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (dbCreator != null) 
+                {
+                    if (!dbCreator.CanConnect())
+                        dbCreator.Create();
+
+                    if (!dbCreator.HasTables())
+                        dbCreator.CreateTables();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ERROR: ProductContext.cs, ProductContext");
+                Console.WriteLine(ex.Message);
+            }
+        }
 
         public DbSet<Product> Products { get; set; }
     }
--- ProductService/Models/Product.cs
+++ ProductService/Models/Product.cs
@@ -1,13 +1,18 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductService.Models
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         [MaxLength(100)]
         public string Name { get; set; }
+        [Required]
         public decimal Price { get; set; }
         [MaxLength(500)]
         public string Description { get; set; }
